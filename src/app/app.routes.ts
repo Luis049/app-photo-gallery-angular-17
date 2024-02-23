080751033329bf9f511d6019c5af43f1a79d0865
Luis <ariza.9485@gmail.com>
2024-02-22 23:55:39 -0500

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: GalleryComponent },
+    { path: 'about-me', component: AboutMeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
